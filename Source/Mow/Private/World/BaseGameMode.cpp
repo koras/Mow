d6379125de8c28dfe2d18425b5385cf1c8b6ff71
koras <kos6476@yandex.ru>
2019-02-21 01:27:04 +0300

    create new base character

--- Source/Mow/Private/World/BaseGameMode.cpp
+++ Source/Mow/Private/World/BaseGameMode.cpp
@@ -1,14 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+ 
+#include "BaseGameMode.h" 
 
-#include "BaseGameMode.h"
 
 ABaseGameMode::ABaseGameMode()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	PlayerControllerClass = AWindController::StaticClass();
-	DefaultPawnClass = AManagerCamera::StaticClass();
+	PlayerControllerClass = AWindController::StaticClass(); 
+	DefaultPawnClass = AManagerCamera::StaticClass(); 
 }
-
 void ABaseGameMode::BeginPlay()
 {
 	Super::BeginPlay();
